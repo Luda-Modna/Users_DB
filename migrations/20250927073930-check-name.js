91752775d1cc4ecb6a549f20ee5d269d22d41dda
Luda Modna <ludmilamodna3@gmail.com>
2025-09-29 16:21:47 +0300

    Fix constraint

--- migrations/20250927073930-check-name.js
+++ migrations/20250927073930-check-name.js
@@ -14,6 +14,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('users');
+    await queryInterface.removeConstraint('users', 'first_name_check');
   },
 };
