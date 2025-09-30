d67e4d62a441a8d9dc5c1b2217de9881dbe994c7
Luda Modna <ludmilamodna3@gmail.com>
2025-09-30 16:04:13 +0300

    Add controller + passwHash

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,30 @@
-module.exports.createUser = async (req, res, next) => {};
+const { hashSync } = require('bcrypt');
+const _ = require('lodash');
+const { User } = require('./../models');
+
+const HASH_SALT = 10;
+
+module.exports.createUser = async (req, res, next) => {
+  const { body } = req;
+  try {
+    body.passwHash = hashSync(body.passwHash, HASH_SALT);
+    const createdUser = await User.create(body);
+    if (!createdUser) {
+      return res.status(400).send('ERROR');
+    }
+
+    const preparedUser = _.omit(createdUser.get(), [
+      'passwHash',
+      'createdAt',
+      'updatedAt',
+    ]);
+
+    res.status(201).send(preparedUser);
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports.getUser = async (req, res, next) => {};
 module.exports.getUserById = async (req, res, next) => {};
 module.exports.updateUserById = async (req, res, next) => {};
