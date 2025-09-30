9d252100f8a9a1ce43d0bd446a7a04b2fe18b1e6
Luda Modna <ludmilamodna3@gmail.com>
2025-09-30 14:54:39 +0300

    Add base contrillers

---
+++ controllers/usersController.js
@@ -0,0 +1,5 @@
+module.exports.createUser = async (req, res, next) => {};
+module.exports.getUser = async (req, res, next) => {};
+module.exports.getUserById = async (req, res, next) => {};
+module.exports.updateUserById = async (req, res, next) => {};
+module.exports.deleteUserById = async (req, res, next) => {};
