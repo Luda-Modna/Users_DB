7d863351b6af5ffe25a1ecb6b5c1305c6cb06cd1
Luda Modna <ludmilamodna3@gmail.com>
2025-10-14 16:05:14 +0300

    Add storage +  fileFilter

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,6 +2,21 @@ const path = require('path');
 const multer = require('multer');
 const { STATIC_PATH } = require('./../constants');
 
-const upload = multer({ dest: path.join(STATIC_PATH, 'images') });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(STATIC_PATH, 'images'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  },
+});
+
+function fileFilter (req, file, cb) {
+  const MIME_TYPE_REG_EXP = /^image\/(gif|jpeg|png)$/;
+
+  cb(null, MIME_TYPE_REG_EXP.test(file.mimetype));
+}
+
+const upload = multer({ storage, fileFilter });
 
 module.exports.uploadUserPhoto = upload.single('userPhoto');
